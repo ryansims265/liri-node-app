8255ece50f8d0fc69bdff09eeed8c385d2ea79af
Ryan Sims <ryan@Ryans-MacBook-Pro.local>
2019-08-08 22:51:27 -0400

    Fixed spotify api and added code comments

--- liri.js
+++ liri.js
@@ -1,3 +1,4 @@
+//First I add all of the required packages 
 require("dotenv").config();
 var keys = require("./keys");
 var Spotify = require("node-spotify-api");
@@ -11,25 +12,16 @@ var moment = require('moment');
 const colors = require('colors');
 var request = require("request");
 
-fs.appendFileSync("random.txt", "Starting Here");
-
-// console.log(keys.spotify.secret);
-
-
-
-moment().format();
-
-// var SPOTIFY_ID = "0fe7fd97619c4e14a9a07efc57353c7f";
-// var SPOTIFY_SECRET = "34939e78c9df41e1a711e9df0535d28c";
-
+//Next I set up the command line inputs as variables 
 var userOperator = process.argv[2];
 var userSelector = process.argv[3];
 
+//Then we create a loop to take in multi-word inputs without breaking the app
 for (var i = 4; i < process.argv.length; i++) {
     userSelector += '+' + process.argv[i];
 }
 
-
+//Then we log the operator and selector 
 console.log(colors.red("The Operator is " + process.argv[2]));
 console.log(colors.red("The Selector is " + userSelector));
 
@@ -37,35 +29,42 @@ console.log(colors.red("The Selector is " + userSelector));
 
 
 
-
+//First up is the OMDB API stored as a function 
 var movieThis = function () {
     //Assign the command line input to a variable to be passed to the query 
     var movieName = userSelector;
     // Build the query url with our new command line variable
     var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&tomatoes=true&apikey=trilogy";
-
+//Then we have the actual request 
     request(queryUrl, function (error, response, body) {
 
-        // If the request is successful = 200
+        
         if (!error && response.statusCode === 200) {
             var body = JSON.parse(body);
-
-            //Simultaneously output to console and log.txt via NPM simple-node-logger
-            console.log('================ Movie ================');
-            console.log("Title: " + body.Title);
-            console.log("Release Year: " + body.Year);
-            console.log("IMdB Rating: " + body.imdbRating);
-            console.log("Rotten Tomatoes Rating: " + body.Ratings[2].Value);
-            console.log("Country: " + body.Country);
-            console.log("Language: " + body.Language);
-            console.log("Plot: " + body.Plot);
-            console.log("Actors: " + body.Actors);
-            console.log('===================================');
-
+//We are storing the response data in a variable called showData
+            var showData = [
+            console.log('================ Movie ================'),
+            console.log("Title: " + body.Title),
+            console.log("Release Year: " + body.Year),
+            console.log("IMdB Rating: " + body.imdbRating),
+            console.log("Rotten Tomatoes Rating: " + body.Ratings[2].Value),
+            console.log("Country: " + body.Country),
+            console.log("Language: " + body.Language),
+            console.log("Plot: " + body.Plot),
+            console.log("Actors: " + body.Actors),
+            console.log('==================================='),
+            ]
+//We are then appending this newly defined object to the text file as an audit long 
+            fs.appendFile("random.txt", showData, function(err) {
+                if (err) throw err;
+                console.log(showData);
+              });
+//Error handeling 
         } else {
             //else - throw error
             console.log("Error occurred.")
         }
+        //If the user does not input any movie then we make a suggestion on Netflix 
         //Response if user does not type in a movie title
         if (movieName === "") {
             console.log("-----------------------");
@@ -74,48 +73,74 @@ var movieThis = function () {
             console.log("-----------------------");
         }
     });
+    
 }
-
+//Next up is the spotify API  
 var spotifyThisSong = function (songName) {
-
+//We are using spotify.search as our query since we installed the spotify package 
     spotify.search(
         {
             type: "track",
             query: userSelector
         },
+        //Error handeling
         function (err, data) {
             if (err) {
                 console.log("Error occurred: " + err);
                 return;
             }
-
+//This varibles sets songs equal to the response from spotify 
             var songs = data.tracks.items;
+//We then store out responses to be logged within an object so we can push the audit trail to the text file 
+            var showData = [
+                console.log(i),
+                console.log("-----------------------------------"),
+                console.log("Artist: " + songs[i].artists[i].name),
+                console.log("Song Name: " + songs[i].name),
+                console.log("Preview: " + songs[i].preview_url),
+                console.log("Album: " + songs[i].album.name),
+                console.log("-----------------------------------")
+            ]
+            //We are then appending this newly defined object to the text file as an audit long 
 
-            for (var i = 0; i < 1; i++) {
-                console.log(i);
-                console.log("-----------------------------------");
-                console.log("Artist: " + songs[i].artists[i].name);
-                console.log("Song Name: " + songs[i].name);
-                console.log("Preview: " + songs[i].preview_url);
-                console.log("Album: " + songs[i].album.name);
-                console.log("-----------------------------------");
-            }
+            fs.appendFile("random.txt", function(err) {
+                if (err) throw err;
+                console.log(showData);
+              });
+            
         }
     );
+    
 };
-
+//Lastly is the concert API call 
 var concertThis = function (artistName) {
-    var artist = process.argv[3];
+    //First we set artist equal to the user input which can be mutil-word 
+    var artist = userSelector;
     
     var bandsquery = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
-
+//Then we query using our newly defined artist variable 
     axios.get(bandsquery)
   .then(function (response) {
-    console.log(colors.blue("The venue name is " + response.data[1].venue.name));
-    console.log(colors.blue("THe venue location is " + response.data[1].venue.city + ", " + response.data[1].venue.region));
-    console.log(colors.blue("The date of the concert is " + response.data[1].datetime));
-  })
+      //Then we use axios to call the api 
+    var showData = [
+    console.log(colors.blue("The venue name is " + response.data[1].venue.name)),
+    console.log(colors.blue("THe venue location is " + response.data[1].venue.city + ", " + response.data[1].venue.region)),
+    console.log(colors.blue("The date of the concert is " + response.data[1].datetime)),
+    //Lastly we store all of the information to be logged to a object so that we can push it to the text file 
+].join("\n\n");
+//Include 2 new lines for double spacing
+
+
+//We are then appending this newly defined object to the text file as an audit long 
+fs.appendFile("random.txt", showData, function(err) {
+    if (err) throw err;
+    console.log(showData);
+  });
+  });
+  
 }
+
+
 
 
 
@@ -123,25 +148,29 @@ var concertThis = function (artistName) {
 var runLiri = function () {
     if (process.argv[2] === "concert-this") {
         concertThis();
+        //Concert API
 
     };
     if (process.argv[2] === "spotify-this-song") {
+        //Spotify API
         if (userSelector === undefined){
             userSelector = "The Sign";
+            //Default Song
         }
         spotifyThisSong();
     }
     if (process.argv[2] === "movie-this") {
         movieThis();
+        //Movie API
     }
     if (process.argv[2] === "undefined"){
         console.log("You have to choose a function")
         console.log("Here are your choices: movie-this, concert-this, or spotify-this-song")
+        //Alerts the user that they are stupid 
+
     }
+   
+    
 };
 runLiri();
-
-
-
-// var term = process.argv.slice[3].join(" ");
-// console.log(term);
+//Runs the conditional logic which launches the whole application 
